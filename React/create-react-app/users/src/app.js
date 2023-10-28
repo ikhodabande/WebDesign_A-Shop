acff6e1e3a2395045684b6bee458d55108553f21
Amirmohammad <amii_mhmd@yahoo.com>
2023-10-28 23:17:06 +0330

    added src

---
+++ React/create-react-app/users/src/app.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Users from './components/users'
+import Navbar from './components/navbar';
+import { Route, Routes, redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import User from './components/user';
+import Not_found from './components/not-found';
+
+
+class App extends Component {
+    state = { } 
+    render() { 
+        return (
+        <>
+        <Navbar />
+        <div className='container mt-3'>
+            <Routes>
+             <Route path='/users/:id' Component={User} />
+             <Route path='/users' Component={Users} />
+             <Route path='/login' Component={Login} />
+             <Route path='/not-found' Component={Not_found} />
+             <Route path='/register' Component={Register} />
+             <Route path='/' Component={Home} />
+            <Route path='*' 
+             element={<Not_found />} />
+            </Routes>
+           
+        </div>
+        </>
+        );
+    }
+}
+ 
+export default App;
---
+++ React/create-react-app/users/src/components/not-found.jsx
@@ -0,0 +1,5 @@
+const Not_found = () => {
+    return ( <h1>Not Founded Page</h1> );
+}
+ 
+export default Not_found;
