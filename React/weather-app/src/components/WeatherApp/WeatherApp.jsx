73cec2b7f2acdbe6f3a30791ab2e26004dd45a91
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-07 16:50:32 +0330

    updated

---
+++ React/weather-app/src/components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './WeatherApp.css';
+import search_icon from '../Assests/search.png';
+import clear_icon from '../Assests/clear.png';
+import clouds_icon from '../Assests/clouds.png';
+import drizzle_icon from '../Assests/drizzle.png';
+import humidity_icon from '../Assests/humidity.png';
+import mist_icon from '../Assests/mist.png';
+import rain_icon from '../Assests/rain.png';
+import snow_icon from '../Assests/snow.png';
+import wind_icon from '../Assests/snow.png';
+
+
+const WeatherApp = () => {
+  return (
+    <div className='container'>
+
+    </div>
+  )
+}
+
+export default WeatherApp;
