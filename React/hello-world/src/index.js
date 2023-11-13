68f86f4f123d20bd04cc57ce063ed565d91b90fc
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-13 12:06:26 +0330

    delet old files and update

--- React/hello-world/src/index.js
+++
@@ -1,7 +0,0 @@
-import App from './app';
-// import App from './components/functional/app';
-import ReactDom from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-ReactDom.render(<App/>,document.getElementById('root'));
